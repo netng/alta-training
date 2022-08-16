553e5441d7affb388b357b53c47594a503fbf9ad
nan <net.nandang@gmail.com>
2022-08-17 00:12:15 +0700

    updated/-- adding validation when empty argument for createRootFolder

--- day-2/tasks/cli/problems/2-create-automation-script/automate.sh
+++ day-2/tasks/cli/problems/2-create-automation-script/automate.sh
@@ -7,20 +7,28 @@ ABOUT_ME_PERSONAL="personal"
 ABOUT_ME_PROFESSIONAL="professional"
 MY_FRIENDS="my_friends"
 MY_SYSTEM_INFO="my_system_info"
+FB_USERNAME=$2
+IN_USERNAME=$3
 
 
 createRootFolder () {
-    mkdir "$1 $FOLDER_DATE"
+    if [[ $1 -eq 0 ]]; then
+        echo "Please type yourname after ./automate command. Ex. ./automate nandang"
+        exit 1;
+    else
+        mkdir "$1 $FOLDER_DATE"
+    fi
 }
 
 createAboutMe () {
     cd "$ROOT_FOLDER_NAME $FOLDER_DATE"
     mkdir -p "$ABOUT_ME/$ABOUT_ME_PERSONAL" && mkdir -p "$ABOUT_ME/$ABOUT_ME_PROFESSIONAL"
+
 }
 
 main () {
     createRootFolder $ROOT_FOLDER_NAME
-    createAboutMe
+    createAboutMe $FB_USERNAME $IN_USERNAME
 }
 
 main
