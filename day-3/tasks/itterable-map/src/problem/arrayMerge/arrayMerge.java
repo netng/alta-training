91b69a62bc3a8b0b16c0e01f01c6c775dfbc6e10
nan <net.nandang@gmail.com>
2022-08-18 15:28:27 +0700

    update/-- change HashSet to LinkedHasSet to  retain their order

--- day-3/tasks/itterable-map/src/problem/arrayMerge/arrayMerge.java
+++ day-3/tasks/itterable-map/src/problem/arrayMerge/arrayMerge.java
@@ -3,19 +3,22 @@ package problem.arrayMerge;
 import java.sql.Array;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class arrayMerge {
     public static void main(String[] args) {
-        String[] array1 = {"ujang", "udin", "asep"};
-        String[] array2 = {"ujang", "aep", "asep", "abrur"};
+        String[] array1 = {"kazuya", "jin", "lee"};
+        String[] array2 = {"kazuya", "feng"};
         mergeArrayAsUnique(array1, array2);
     }
 
     static void mergeArrayAsUnique(String[] array1, String[] array2) {
         String[] mergedArray = Arrays.copyOf(array1, array1.length + array2.length);
         System.arraycopy(array2, 0, mergedArray, array1.length, array2.length);
+        System.out.println(Arrays.toString(mergedArray));
 
-        String[] uniqueArray = new HashSet<String>(Arrays.asList(mergedArray)).toArray(new String[0]);
-        System.out.println(Arrays.toString(uniqueArray));
+        Set<String> uniqueArray = new LinkedHashSet<>(Arrays.asList(mergedArray));
+        System.out.println(uniqueArray);
     }
 }
