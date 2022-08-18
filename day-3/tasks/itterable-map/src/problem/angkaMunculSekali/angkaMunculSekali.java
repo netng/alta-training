827c81fd7fd0f5dadded88409bab0d64dada8387
nan <net.nandang@gmail.com>
2022-08-18 11:27:20 +0700

    create/-- class angkaMunculSekali

---
+++ day-3/tasks/itterable-map/src/problem/angkaMunculSekali/angkaMunculSekali.java
@@ -0,0 +1,14 @@
+package problem.angkaMunculSekali;
+
+import java.util.Arrays;
+
+public class angkaMunculSekali {
+    public static void main(String[] args) {
+
+    }
+
+    static void getOneEachSameNumber(String number) {
+        String[] numArray = number.split(",");
+        System.out.println(Arrays.asList(numArray));
+    }
+}
