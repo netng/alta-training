dfc5b16eee1c9de8ae29adaef010ab3ccb32a4f1
nan <net.nandang@gmail.com>
2022-08-18 20:18:35 +0700

    create/-- vokalKonsonan

---
+++ day-2/tasks/spring-java-basic/src/VokalKonsonan.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+
+public class VokalKonsonan {
+    public static void main(String[] args) {
+        countVowelConsonant("Alterra Indonesia");
+    }
+
+    static void countVowelConsonant (String word) {
+        char[] charOfWord = word.replaceAll("\\s", "").toLowerCase().toCharArray();
+        Integer numberOfVowel = 0;
+        Integer numberOfConsonant = 0;
+        Integer numberOfCharacter = charOfWord.length ;
+
+        for (var charWord : charOfWord) {
+            switch (charWord) {
+                case 'a':
+                case 'i':
+                case 'u':
+                case 'e':
+                case 'o':
+                    numberOfVowel += 1;
+                    break;
+                default:
+                    numberOfConsonant += 1;
+            }
+        }
+
+        System.out.println(String.format("Jumlah Vokal: %s", numberOfVowel));
+        System.out.println(String.format("Jumlah Konsonan: %s", numberOfConsonant));
+        System.out.println(String.format("Total Karakter: %s", numberOfCharacter));
+
+    }
+}
