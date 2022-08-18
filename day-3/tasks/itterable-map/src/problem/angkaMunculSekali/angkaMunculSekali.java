ba6eb90c15dc21c8ba2ab9104b678a7e5aeb5bbb
nan <net.nandang@gmail.com>
2022-08-18 14:40:56 +0700

    update/-- belajar menggunakan hashMap, walau belum tau mengoptimalkannya seperti apa

--- day-3/tasks/itterable-map/src/problem/angkaMunculSekali/angkaMunculSekali.java
+++ day-3/tasks/itterable-map/src/problem/angkaMunculSekali/angkaMunculSekali.java
@@ -1,14 +1,39 @@
 package problem.angkaMunculSekali;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class angkaMunculSekali {
     public static void main(String[] args) {
 
+        getOneEachSameNumber("75623752");
+        getOneEachSameNumber("1122");
     }
 
     static void getOneEachSameNumber(String number) {
-        String[] numArray = number.split(",");
-        System.out.println(Arrays.asList(numArray));
+        String[] arrString = new String[number.length()];
+        arrString = number.split("");
+
+        List<String> list = new ArrayList<>();
+
+        HashMap<Integer, String> hashMap = new HashMap<>();
+
+        for (int i = 0; i < arrString.length; i++) {
+            hashMap.put(i, arrString[i]);
+        }
+
+        Map<String, Long> values = hashMap.values().stream().collect(
+                Collectors.groupingBy(Function.identity(), Collectors.counting())
+        );
+
+        for (Map.Entry<String, Long> value : values.entrySet()) {
+            if (value.getValue() == 1) {
+                list.add(String.valueOf(value.getKey()));
+            }
+        }
+
+        System.out.println(list);
+
     }
 }
