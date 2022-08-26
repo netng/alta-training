70bddcfec60e019af2dfb51b6ec1214f260eb567
netng <net.nandang@gmail.com>
2022-08-26 08:52:56 +0700

    create/-- initial setup for tasks springboot jpa crud basic day 9

---
+++ day-9/tasks/intro-springboot-jpa/src/main/java/com/training/alterra/introspringbootjpa/IntroSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.training.alterra.introspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ day-9/tasks/intro-springboot-jpa/src/test/java/com/training/alterra/introspringbootjpa/IntroSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.alterra.introspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
