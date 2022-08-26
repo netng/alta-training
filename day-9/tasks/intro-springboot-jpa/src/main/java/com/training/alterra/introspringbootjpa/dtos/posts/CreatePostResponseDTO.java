ea508ae6edf5878213123909d3c15cee9f553025
netng <net.nandang@gmail.com>
2022-08-26 11:28:54 +0700

    craete/-- create dtos post

---
+++ day-9/tasks/intro-springboot-jpa/src/main/java/com/training/alterra/introspringbootjpa/dtos/posts/CreatePostResponseDTO.java
@@ -0,0 +1,25 @@
+package com.training.alterra.introspringbootjpa.dtos.posts;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+public class CreatePostResponseDTO implements Serializable {
+    private Long id;
+
+    private String content;
+
+    @JsonIgnore
+    @JsonProperty(value = "created_at")
+    private Date createdAt;
+
+    @JsonIgnore
+    @JsonProperty(value = "updated_at")
+    private Date updatedAt;
+}
