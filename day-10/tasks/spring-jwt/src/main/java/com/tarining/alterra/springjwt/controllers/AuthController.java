a64d57f58e1d8744e61f4e39a659efaa772a6395
netng <net.nandang@gmail.com>
2022-08-29 17:01:27 +0700

    update

--- day-10/tasks/spring-jwt/src/main/java/com/tarining/alterra/springjwt/controllers/AuthController.java
+++ day-10/tasks/spring-jwt/src/main/java/com/tarining/alterra/springjwt/controllers/AuthController.java
@@ -1,10 +1,13 @@
 package com.tarining.alterra.springjwt.controllers;
 
 import com.tarining.alterra.springjwt.entities.AuthRequest;
+import com.tarining.alterra.springjwt.entities.User;
+import com.tarining.alterra.springjwt.repositories.UserRepository;
 import com.tarining.alterra.springjwt.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,13 +16,10 @@ public class AuthController {
     @Autowired
     private JwtUtil jwtUtil;
 
+    private UserRepository userRepository;
+
     @Autowired
     private AuthenticationManager authenticationManager;
-
-    @GetMapping("/")
-    public String welcome() {
-        return "Hello";
-    }
 
     @PostMapping("/auth/login")
     public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
@@ -33,5 +33,11 @@ public class AuthController {
             throw new Exception("invalid phone or password");
         }
         return jwtUtil.generateToken(authRequest.getPhone());
+    }
+
+    @GetMapping("/auth/info")
+    public User getUserInfo() {
+        String phone = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userRepository.findByPhone(phone);
     }
 }
