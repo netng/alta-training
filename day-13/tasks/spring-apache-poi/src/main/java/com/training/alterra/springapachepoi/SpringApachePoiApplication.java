b99dfc97c5c60fd399bdb180beb72734e00dc28b
netng <net.nandang@gmail.com>
2022-09-01 08:56:21 +0700

    initialize/-- initial setup for tasks day-13 spring apache poi

---
+++ day-13/tasks/spring-apache-poi/src/main/java/com/training/alterra/springapachepoi/SpringApachePoiApplication.java
@@ -0,0 +1,13 @@
+package com.training.alterra.springapachepoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApachePoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApachePoiApplication.class, args);
+	}
+
+}
---
+++ day-13/tasks/spring-apache-poi/src/test/java/com/training/alterra/springapachepoi/SpringApachePoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.alterra.springapachepoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApachePoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
