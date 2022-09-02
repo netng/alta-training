5a1097ff348a8a140b285df246c45021725c92a9
netng <net.nandang@gmail.com>
2022-09-02 16:08:48 +0700

    initialize/-- initial setup task day-14

---
+++ day-14/tasks/spring-queue-rabbitmq/src/main/java/com/training/alterra/springqueuerabbitmq/SpringQueueRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.training.alterra.springqueuerabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQueueRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQueueRabbitmqApplication.class, args);
+	}
+
+}
---
+++ day-14/tasks/spring-queue-rabbitmq/src/test/java/com/training/alterra/springqueuerabbitmq/SpringQueueRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.alterra.springqueuerabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQueueRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
